c2fa8e5b62b9d5ee5affa70f3de234f233574e53
Levon Ghukasyan <levon_ghukasyian@horizon-agency.com>
2021-09-30 20:20:06 +0400

    changed compat folder

--- src/compat/strnlen.cpp
+++ src/compat/strnlen.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/helleniccoin-config.h"
+#include <config/helleniccoin-config.h>
 #endif
 
 #include <cstring>
