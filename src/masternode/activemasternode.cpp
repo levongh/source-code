22eb72672b0cfea0e3e3ff5ca455f7abf6aca02e
Levon Ghukasyan <levon_ghukasyian@horizon-agency.com>
2021-09-30 19:33:52 +0400

    1. updated masternode logic 2. moved masternode files into separate folder

--- src/masternode/activemasternode.cpp
+++ src/masternode/activemasternode.cpp
@@ -3,14 +3,12 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "activemasternode.h"
-#include "evo/deterministicmns.h"
-#include "init.h"
-#include "masternode-sync.h"
-#include "netbase.h"
-#include "protocol.h"
-#include "validation.h"
-#include "warnings.h"
+#include <masternode/activemasternode.h>
+#include <masternode/masternode-sync.h>
+#include <netbase.h>
+#include <protocol.h>
+#include <validation.h>
+#include <warnings.h>
 
 // Keep track of the active Masternode
 CActiveMasternodeInfo activeMasternodeInfo;
@@ -27,6 +25,8 @@ std::string CActiveMasternodeManager::GetStateString() const
         return "REMOVED";
     case MASTERNODE_OPERATOR_KEY_CHANGED:
         return "OPERATOR_KEY_CHANGED";
+    case MASTERNODE_PROTX_IP_CHANGED:
+        return "PROTX_IP_CHANGED";
     case MASTERNODE_READY:
         return "READY";
     case MASTERNODE_ERROR:
@@ -47,6 +47,8 @@ std::string CActiveMasternodeManager::GetStatus() const
         return "Masternode removed from list";
     case MASTERNODE_OPERATOR_KEY_CHANGED:
         return "Operator key changed or revoked";
+    case MASTERNODE_PROTX_IP_CHANGED:
+        return "IP address specified in ProTx changed";
     case MASTERNODE_READY:
         return "Ready";
     case MASTERNODE_ERROR:
@@ -56,20 +58,20 @@ std::string CActiveMasternodeManager::GetStatus() const
     }
 }
 
-void CActiveMasternodeManager::Init()
+void CActiveMasternodeManager::Init(const CBlockIndex* pindex)
 {
     LOCK(cs_main);
 
     if (!fMasternodeMode) return;
 
-    if (!deterministicMNManager->IsDIP3Enforced()) return;
+    if (!deterministicMNManager->IsDIP3Enforced(pindex->nHeight)) return;
 
     // Check that our local network configuration is correct
-    if (!fListen) {
-        // listen option is probably overwritten by smth else, no good
+    if (!fListen && Params().RequireRoutableExternalIP()) {
+        // listen option is probably overwritten by something else, no good
         state = MASTERNODE_ERROR;
         strError = "Masternode must accept connections from outside. Make sure listen configuration option is not overwritten by some another parameter.";
-        LogPrintf("CActiveDeterministicMasternodeManager::Init -- ERROR: %s\n", strError);
+        LogPrintf("CActiveMasternodeManager::Init -- ERROR: %s\n", strError);
         return;
     }
 
@@ -78,7 +80,7 @@ void CActiveMasternodeManager::Init()
         return;
     }
 
-    CDeterministicMNList mnList = deterministicMNManager->GetListAtChainTip();
+    CDeterministicMNList mnList = deterministicMNManager->GetListForBlock(pindex);
 
     CDeterministicMNCPtr dmn = mnList.GetMNByOperatorKey(*activeMasternodeInfo.blsPubKeyOperator);
     if (!dmn) {
@@ -95,34 +97,36 @@ void CActiveMasternodeManager::Init()
         return;
     }
 
-    mnListEntry = dmn;
+    LogPrintf("CActiveMasternodeManager::Init -- proTxHash=%s, proTx=%s\n", dmn->proTxHash.ToString(), dmn->ToString());
 
-    LogPrintf("CActiveMasternodeManager::Init -- proTxHash=%s, proTx=%s\n", mnListEntry->proTxHash.ToString(), mnListEntry->ToString());
-
-    if (activeMasternodeInfo.service != mnListEntry->pdmnState->addr) {
+    if (activeMasternodeInfo.service != dmn->pdmnState->addr) {
         state = MASTERNODE_ERROR;
         strError = "Local address does not match the address from ProTx";
-        LogPrintf("CActiveMasternodeManager::Init -- ERROR: %s", strError);
+        LogPrintf("CActiveMasternodeManager::Init -- ERROR: %s\n", strError);
         return;
     }
 
-    if (Params().NetworkIDString() != CBaseChainParams::REGTEST) {
-        // Check socket connectivity
-        LogPrintf("CActiveDeterministicMasternodeManager::Init -- Checking inbound connection to '%s'\n", activeMasternodeInfo.service.ToString());
-        SOCKET hSocket;
-        bool fConnected = ConnectSocket(activeMasternodeInfo.service, hSocket, nConnectTimeout) && IsSelectableSocket(hSocket);
-        CloseSocket(hSocket);
+    // Check socket connectivity
+    LogPrintf("CActiveMasternodeManager::Init -- Checking inbound connection to '%s'\n", activeMasternodeInfo.service.ToString());
+    SOCKET hSocket = CreateSocket(activeMasternodeInfo.service);
+    if (hSocket == INVALID_SOCKET) {
+        state = MASTERNODE_ERROR;
+        strError = "Could not create socket to connect to " + activeMasternodeInfo.service.ToString();
+        LogPrintf("CActiveMasternodeManager::Init -- ERROR: %s\n", strError);
+        return;
+    }
+    bool fConnected = ConnectSocketDirectly(activeMasternodeInfo.service, hSocket, nConnectTimeout, true) && IsSelectableSocket(hSocket);
+    CloseSocket(hSocket);
 
-        if (!fConnected) {
-            state = MASTERNODE_ERROR;
-            strError = "Could not connect to " + activeMasternodeInfo.service.ToString();
-            LogPrintf("CActiveDeterministicMasternodeManager::Init -- ERROR: %s\n", strError);
-            return;
-        }
+    if (!fConnected && Params().RequireRoutableExternalIP()) {
+        state = MASTERNODE_ERROR;
+        strError = "Could not connect to " + activeMasternodeInfo.service.ToString();
+        LogPrintf("CActiveMasternodeManager::Init -- ERROR: %s\n", strError);
+        return;
     }
 
-    activeMasternodeInfo.proTxHash = mnListEntry->proTxHash;
-    activeMasternodeInfo.outpoint = mnListEntry->collateralOutpoint;
+    activeMasternodeInfo.proTxHash = dmn->proTxHash;
+    activeMasternodeInfo.outpoint = dmn->collateralOutpoint;
     state = MASTERNODE_READY;
 }
 
@@ -135,33 +139,57 @@ void CActiveMasternodeManager::UpdatedBlockTip(const CBlockIndex* pindexNew, con
     if (!deterministicMNManager->IsDIP3Enforced(pindexNew->nHeight)) return;
 
     if (state == MASTERNODE_READY) {
-        auto mnList = deterministicMNManager->GetListForBlock(pindexNew->GetBlockHash());
-        if (!mnList.IsMNValid(mnListEntry->proTxHash)) {
+        auto oldMNList = deterministicMNManager->GetListForBlock(pindexNew->pprev);
+        auto newMNList = deterministicMNManager->GetListForBlock(pindexNew);
+        if (!newMNList.IsMNValid(activeMasternodeInfo.proTxHash)) {
             // MN disappeared from MN list
             state = MASTERNODE_REMOVED;
             activeMasternodeInfo.proTxHash = uint256();
             activeMasternodeInfo.outpoint.SetNull();
             // MN might have reappeared in same block with a new ProTx
-            Init();
-        } else if (mnList.GetMN(mnListEntry->proTxHash)->pdmnState->pubKeyOperator != mnListEntry->pdmnState->pubKeyOperator) {
+            Init(pindexNew);
+            return;
+        }
+
+        auto oldDmn = oldMNList.GetMN(activeMasternodeInfo.proTxHash);
+        auto newDmn = newMNList.GetMN(activeMasternodeInfo.proTxHash);
+        if (newDmn->pdmnState->pubKeyOperator != oldDmn->pdmnState->pubKeyOperator) {
             // MN operator key changed or revoked
             state = MASTERNODE_OPERATOR_KEY_CHANGED;
             activeMasternodeInfo.proTxHash = uint256();
             activeMasternodeInfo.outpoint.SetNull();
             // MN might have reappeared in same block with a new ProTx
-            Init();
+            Init(pindexNew);
+            return;
+        }
+
+        if (newDmn->pdmnState->addr != oldDmn->pdmnState->addr) {
+            // MN IP changed
+            state = MASTERNODE_PROTX_IP_CHANGED;
+            activeMasternodeInfo.proTxHash = uint256();
+            activeMasternodeInfo.outpoint.SetNull();
+            Init(pindexNew);
+            return;
         }
     } else {
-        // MN might have (re)appeared with a new ProTx or we've found some peers and figured out our local address
-        Init();
+        // MN might have (re)appeared with a new ProTx or we've found some peers
+        // and figured out our local address
+        Init(pindexNew);
     }
 }
 
 bool CActiveMasternodeManager::GetLocalAddress(CService& addrRet)
 {
-    // First try to find whatever local address is specified by externalip option
-    bool fFoundLocal = GetLocal(addrRet) && IsValidNetAddr(addrRet);
-    if (!fFoundLocal && Params().NetworkIDString() == CBaseChainParams::REGTEST) {
+    // First try to find whatever our own local address is known internally.
+    // Addresses could be specified via externalip or bind option, discovered via UPnP
+    // or added by TorController. Use some random dummy IPv4 peer to prefer the one
+    // reachable via IPv4.
+    CNetAddr addrDummyPeer;
+    bool fFoundLocal{false};
+    if (LookupHost("8.8.8.8", addrDummyPeer, false)) {
+        fFoundLocal = GetLocal(addrRet, &addrDummyPeer) && IsValidNetAddr(addrRet);
+    }
+    if (!fFoundLocal && !Params().RequireRoutableExternalIP()) {
         if (Lookup("127.0.0.1", addrRet, GetListenPort(), false)) {
             fFoundLocal = true;
         }
@@ -189,6 +217,6 @@ bool CActiveMasternodeManager::IsValidNetAddr(CService addrIn)
 {
     // TODO: regtest is fine with any addresses for now,
     // should probably be a bit smarter if one day we start to implement tests for this
-    return Params().NetworkIDString() == CBaseChainParams::REGTEST ||
+    return !Params().RequireRoutableExternalIP() ||
            (addrIn.IsIPv4() && IsReachable(addrIn) && addrIn.IsRoutable());
 }
--- src/masternode/masternode-meta.h
+++ src/masternode/masternode-meta.h
@@ -3,12 +3,14 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef MASTERNODE_META_H
-#define MASTERNODE_META_H
+#ifndef BITCOIN_MASTERNODE_MASTERNODE_META_H
+#define BITCOIN_MASTERNODE_MASTERNODE_META_H
 
-#include "serialize.h"
+#include <serialize.h>
 
-#include "evo/deterministicmns.h"
+#include <evo/deterministicmns.h>
+
+#include <univalue.h>
 
 #include <memory>
 
@@ -34,21 +36,20 @@ private:
     // KEEP TRACK OF GOVERNANCE ITEMS EACH MASTERNODE HAS VOTE UPON FOR RECALCULATION
     std::map<uint256, int> mapGovernanceObjectsVotedOn;
 
+    int64_t lastOutboundAttempt = 0;
+    int64_t lastOutboundSuccess = 0;
+
 public:
-    CMasternodeMetaInfo() {}
-    CMasternodeMetaInfo(const uint256& _proTxHash) : proTxHash(_proTxHash) {}
+    CMasternodeMetaInfo() = default;
+    explicit CMasternodeMetaInfo(const uint256& _proTxHash) : proTxHash(_proTxHash) {}
     CMasternodeMetaInfo(const CMasternodeMetaInfo& ref) :
         proTxHash(ref.proTxHash),
         nLastDsq(ref.nLastDsq),
         nMixingTxCount(ref.nMixingTxCount),
-        mapGovernanceObjectsVotedOn(ref.mapGovernanceObjectsVotedOn)
+        mapGovernanceObjectsVotedOn(ref.mapGovernanceObjectsVotedOn),
+        lastOutboundAttempt(ref.lastOutboundAttempt),
+        lastOutboundSuccess(ref.lastOutboundSuccess)
     {
-    }
-
-    template <typename Stream>
-    CMasternodeMetaInfo(deserialize_type, Stream& s)
-    {
-        s >> *this;
     }
 
     ADD_SERIALIZE_METHODS
@@ -60,7 +61,11 @@ public:
         READWRITE(nLastDsq);
         READWRITE(nMixingTxCount);
         READWRITE(mapGovernanceObjectsVotedOn);
+        READWRITE(lastOutboundAttempt);
+        READWRITE(lastOutboundSuccess);
     }
+
+    UniValue ToJson() const;
 
 public:
     const uint256& GetProTxHash() const { LOCK(cs); return proTxHash; }
@@ -69,12 +74,15 @@ public:
 
     bool IsValidForMixingTxes() const { return GetMixingTxCount() <= MASTERNODE_MAX_MIXING_TXES; }
 
-    // KEEP TRACK OF EACH GOVERNANCE ITEM INCASE THIS NODE GOES OFFLINE, SO WE CAN RECALC THEIR STATUS
+    // KEEP TRACK OF EACH GOVERNANCE ITEM IN CASE THIS NODE GOES OFFLINE, SO WE CAN RECALCULATE THEIR STATUS
     void AddGovernanceVote(const uint256& nGovernanceObjectHash);
-    // RECALCULATE CACHED STATUS FLAGS FOR ALL AFFECTED OBJECTS
-    void FlagGovernanceItemsAsDirty();
 
     void RemoveGovernanceObject(const uint256& nGovernanceObjectHash);
+
+    void SetLastOutboundAttempt(int64_t t) { LOCK(cs); lastOutboundAttempt = t; }
+    int64_t GetLastOutboundAttempt() const { LOCK(cs); return lastOutboundAttempt; }
+    void SetLastOutboundSuccess(int64_t t) { LOCK(cs); lastOutboundSuccess = t; }
+    int64_t GetLastOutboundSuccess() const { LOCK(cs); return lastOutboundSuccess; }
 };
 typedef std::shared_ptr<CMasternodeMetaInfo> CMasternodeMetaInfoPtr;
 
@@ -88,7 +96,7 @@ private:
     std::map<uint256, CMasternodeMetaInfoPtr> metaInfos;
     std::vector<uint256> vecDirtyGovernanceObjectHashes;
 
-    // keep track of dsq count to prevent masternodes from gaming privatesend queue
+    // keep track of dsq count to prevent masternodes from gaming coinjoin queue
     int64_t nDsqCount = 0;
 
 public:
@@ -101,7 +109,11 @@ public:
 
         std::string strVersion;
         if(ser_action.ForRead()) {
+            Clear();
             READWRITE(strVersion);
+            if (strVersion != SERIALIZATION_VERSION_STRING) {
+                return;
+            }
         }
         else {
             strVersion = SERIALIZATION_VERSION_STRING;
@@ -123,16 +135,13 @@ public:
         }
 
         READWRITE(nDsqCount);
-
-        if(ser_action.ForRead() && (strVersion != SERIALIZATION_VERSION_STRING)) {
-            Clear();
-        }
     }
 
 public:
     CMasternodeMetaInfoPtr GetMetaInfo(const uint256& proTxHash, bool fCreate = true);
 
     int64_t GetDsqCount() { LOCK(cs); return nDsqCount; }
+    int64_t GetDsqThreshold(const uint256& proTxHash, int nMnCount);
 
     void AllowMixing(const uint256& proTxHash);
     void DisallowMixing(const uint256& proTxHash);
@@ -140,7 +149,6 @@ public:
     bool AddGovernanceVote(const uint256& proTxHash, const uint256& nGovernanceObjectHash);
     void RemoveGovernanceObject(const uint256& nGovernanceObjectHash);
 
-    void AddDirtyGovernanceObjectHash(const uint256& nHash);
     std::vector<uint256> GetAndClearDirtyGovernanceObjectHashes();
 
     void Clear();
@@ -151,4 +159,4 @@ public:
 
 extern CMasternodeMetaMan mmetaman;
 
-#endif//MASTERNODE_META_H
+#endif // BITCOIN_MASTERNODE_MASTERNODE_META_H
